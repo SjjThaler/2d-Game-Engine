5a0403ebded494ff94b2b22c5d5fdfdc251541e4
SjjThaler <stefanthaler@protonmail.com>
2026-06-21 22:36:20 +0200

    Add neuroevolution: NN evader, population, and the genetic algorithm
    pong.c now evolves the evader's neural network. Each brain (struct brain:
    weights + biases) drives the evader via a forward pass (weighted sums +
    argmax). main runs a genetic algorithm: a random population, evaluate each
    brain by running a match for its survival fitness, select the champion,
    and breed the next generation as elitist mutated copies. Average fitness
    rises across generations.

--- pong.c
+++ pong.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#define HEIGHT 20
+#define WIDTH 20
+#define OUTPUTS 5
+#define INPUTS 4
+#define POP_SIZE 50
+#define SURVIVAL_LIMIT 1000
+#define GENERATIONS 1000
 
 struct dot {
 	int x;
 	int y;
 
 };
+
+struct brain {
+	float weights[OUTPUTS][INPUTS];
+	float biases[OUTPUTS];
+};
+
+
+struct brain active;		
+
 enum action {
 	UP = 1,
 	DOWN = 2,
@@ -14,9 +31,27 @@ enum action {
 	STAY = 0
 };
 
-enum action opponent_controller (struct dot player, struct dot opponent) {
-	
-	return STAY;	
+
+
+
+
+enum action forward_pass(int inputs[INPUTS]) {
+	float score[5];
+	for(int j = 0; j < OUTPUTS; j++) {
+		score[j] = active.biases[j];
+		for(int i = 0; i < INPUTS; i++) {
+			score[j] += inputs[i] * active.weights[j][i];	
+		}
+	}
+	int best = 0;
+	for (int k = 0; k < OUTPUTS; k++) {
+		if (score[k] > score[best]) {
+			best = k;
+		}
+
+	}
+	enum action nn_move = best;
+	return nn_move;
 }
 
 enum action player_controller (struct dot player, struct dot opponent) {
@@ -45,6 +80,11 @@ enum action player_controller (struct dot player, struct dot opponent) {
 		}
 	}
 }
+enum action opponent_controller (struct dot player, struct dot opponent) {
+	int inputs[INPUTS] = {opponent.x-player.x, opponent.y-player.y, opponent.x, opponent.y}; 
+	enum action nn_move = forward_pass(inputs);	
+	return nn_move;	
+}
 
 void apply_action(struct dot *dot, enum action action ) {
 	switch (action) {
@@ -54,10 +94,45 @@ void apply_action(struct dot *dot, enum action action ) {
 			case LEFT: dot->x -= 1; break;
 			case STAY: break;
 	}
-
+	if (dot->x > WIDTH-1) {
+		dot->x = WIDTH-1;
+	}
+	if (dot->x < 0) {
+		dot->x = 0;
+	}
+	if (dot->y > HEIGHT-1) {
+		dot->y = HEIGHT-1;
+	}
+	if (dot->y < 0) {
+		dot->y = 0;
+	}	
 }
 
-int main (void) {
+void fill_weights_biases(struct brain *b) {
+	for (int i = 0; i < OUTPUTS; i++) {
+		for (int j = 0; j < INPUTS; j++) {
+			
+			b->weights[i][j] = (float)rand() / RAND_MAX * 2 - 1;
+		}
+	}
+	for (int k = 0; k < OUTPUTS; k++) {
+		b->biases[k] = (float)rand() / RAND_MAX * 2 - 1;
+	}
+}
+
+void mutate(struct brain *b) {
+	for (int i = 0; i < OUTPUTS; i++) {
+		for (int j = 0; j < INPUTS; j++) {
+			
+			b->weights[i][j] += ((float)rand() / RAND_MAX * 2 - 1) * 0.1;
+		}
+	}
+	for (int k = 0; k < OUTPUTS; k++) {
+		b->biases[k] += ((float)rand() / RAND_MAX * 2 - 1) * 0.1;
+	}	
+}
+
+int run_match(void) {
 	struct dot player = {
 		0,0,	
 	};
@@ -65,7 +140,7 @@ int main (void) {
 		10,10,
 	};
 	int survival_count = 0;
-	while(survival_count < 10) {
+	while(survival_count < SURVIVAL_LIMIT) {
 
 		enum action opponent_choice = opponent_controller(player, opponent);
 		apply_action(&opponent, opponent_choice);		
@@ -73,12 +148,48 @@ int main (void) {
 		apply_action(&player, player_choice);
 			
 		if (player.x == opponent.x && player.y == opponent.y) {
-			printf("Win!\nOpponent survived for %d rounds!", survival_count);
-			return 0;
+			return survival_count;
 		}
 		survival_count += 1;
-		printf("player: %d,%d\nopponent: %d,%d\n", player.x,player.y,opponent.x,opponent.y);
 	}
-	printf("Game over!\n");
-	return 0;
+
+	return survival_count;
+}
+
+int main (void) {
+		srand(time(NULL));
+		struct brain population[POP_SIZE];
+		int fitness[POP_SIZE];
+		for (int i = 0; i < POP_SIZE; i++) {
+			fill_weights_biases(&population[i]);
+			active = population[i];
+			fitness[i] = run_match();
+		}
+		for(int g = 0; g < GENERATIONS; g++) {
+			for (int p = 0; p < POP_SIZE; p++) {
+				active = population[p];
+				fitness[p] = run_match();
+			}
+			int best = 0;
+			int average = 0;
+			for (int i = 0; i < POP_SIZE; i++) {
+				if (fitness[i] > fitness[best]) {
+					best = i;
+				}
+				average += fitness[i];	
+			}
+			average = average / POP_SIZE;
+			
+
+			struct brain champion = population[best];
+			population[0] = champion;
+				
+			for (int i = 1; i < POP_SIZE; i++) {
+				struct brain mutant = champion;
+				mutate(&mutant);
+				population[i] = mutant;
+			}
+			printf("Average of generation %d: %d\n", g, average);
+		}
+		return 0;
 }
